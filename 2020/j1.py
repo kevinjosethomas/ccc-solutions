90cabc71d4c32d4379d9be7464dec9015d339338
Kevin Thomas <kevin.jt2007@gmail.com>
2021-12-26 01:25:55 -0800

    fix: minor bug fixes

--- 2020/j1.py
+++ 2020/j1.py
@@ -1,6 +1,6 @@
-small_treats = input()
-medium_treats = input()
-large_treats = input()
+small_treats = int(input())
+medium_treats = int(input())
+large_treats = int(input())
 
 happiness = small_treats + (2 * medium_treats) + (3 * large_treats)
 
