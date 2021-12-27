d25a9ac02340d9a9cb80d7032848c26c89c1fbf5
Kevin Thomas <kevin.jt2007@gmail.com>
2021-12-26 20:50:30 -0800

    feat: added solution for j1 2019

---
+++ 2019/j1.py
@@ -0,0 +1,17 @@
+a_three = int(input())
+a_two = int(input())
+a_one = int(input())
+
+b_three = int(input())
+b_two = int(input())
+b_one = int(input())
+
+apples = (a_three * 3) + (a_two * 2) + a_one
+bananas = (b_three * 3) + (b_two * 2) + b_one
+
+if apples > bananas:
+    print("A")
+elif apples < bananas:
+    print("B")
+else:
+    print("T")
